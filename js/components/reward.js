273801e5d2f4b127622eccc4dcf4ffb0f31edafa
Aitor Ledesma <aitorlevi@gmail.com>
2024-12-02 18:03:56 +0100

    Refactor reward.js to add documentation, improve code readability, and enhance error handling

--- js/components/reward.js
+++ js/components/reward.js
@@ -2,6 +2,9 @@ import { updateText } from "../utils/helpers.js";
 import { showAlert } from "../utils/alert.js";
 let codeGenerated = "";
 
+/**
+ * Initializes the observer for the reward container.
+ */
 document.addEventListener("DOMContentLoaded", () => {
   const rewardContainer = document.getElementById("rewardContainer");
   if (rewardContainer) {
@@ -19,6 +22,9 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 });
 
+/**
+ * Handles actions when the reward container is shown.
+ */
 export const onRewardContainerShown = () => {
   fetch("../../formData.json")
     .then((response) => response.json())
@@ -28,13 +34,16 @@ export const onRewardContainerShown = () => {
     .catch((error) => {
       showAlert(
         "error",
-        "Ha habido un problema al cargar los datos. Por favor intentalo de nuevo."
+        "Ha habido un problema al cargar los datos. Por favor inténtalo de nuevo."
       );
     });
   generateCode();
   startTimer();
 };
 
+/**
+ * Generates a reward code and updates the DOM.
+ */
 const generateCode = () => {
   const year = sumLastTwoDigits(parseInt(localStorage.getItem("year")));
   const action = getLastFourChars(localStorage.getItem("action"));
@@ -42,21 +51,46 @@ const generateCode = () => {
   document.getElementById("rewardCode").textContent = codeGenerated;
 };
 
-const sumLastTwoDigits = (number) => {
-  const lastTwoDigits = number % 100;
-  const tens = Math.floor(lastTwoDigits / 10);
-  const units = lastTwoDigits % 10;
-  return tens + units;
+/**
+ * Sums the last two digits of a number.
+ *
+ * @param {number} number - The number to process.
+ * @returns {number} The sum of the last two digits.
+ */
+export const sumLastTwoDigits = (number) => {
+  if (number > 10) {
+    const lastTwoDigits = number % 100;
+    const tens = Math.floor(lastTwoDigits / 10);
+    const units = lastTwoDigits % 10;
+    return tens + units;
+  } else if (number > 0) {
+    return number;
+  } else {
+    return 0;
+  }
 };
 
-const getLastFourChars = (str) => {
-  const filteredStr = str.replace(/[aA\s]/g, "");
-  return filteredStr.slice(-4);
+/**
+ * Gets the last four characters of a string, excluding 'a', 'A', and spaces.
+ *
+ * @param {string} str - The string to process.
+ * @returns {string} The last four characters of the filtered string.
+ */
+export const getLastFourChars = (str) => {
+  if (typeof str === "string") {
+    const filteredStr = str.replace(/[aA\s]/g, "");
+    return filteredStr.slice(-4);
+  } else {
+    return "";
+  }
 };
 
+/**
+ * Starts a countdown timer and updates the DOM.
+ */
 const startTimer = () => {
   const timerElement = document.getElementById("timerText");
-  let timeLeft = 20 * 60;
+  let timeLeft = 0.5 * 60;
 
   const updateTimer = () => {
     const minutes = Math.floor(timeLeft / 60);
@@ -77,6 +111,9 @@ const startTimer = () => {
   updateTimer();
 };
 
+/**
+ * Adds event listeners to buttons for copying code and opening a URL.
+ */
 const listenerButtons = () => {
   const copyButton = document.getElementById("copyButton");
   const sirokoButton = document.getElementById("sirokoButton");
